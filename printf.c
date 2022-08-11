6a0a37e9056c9a4e829e2c01416a2ade3a8e609f
Biruke Lemma <bsisay37@gmail.com>
2022-08-11 10:58:38 +0300

    printf new format

--- printf.c
+++ printf.c
@@ -1,50 +1,90 @@
 #include "main.h"
+
+void cleanup(va_list args, buffer_t *output);
+int run_printf(const char *format, va_list args, buffer_t *output);
+int _printf(const char *format, ...);
+
 /**
- * _printf - prints out strings, ints, decimals
- * strings, reverses strings and converts to
- * binary to standard output
- * @format: initial string to print out
- * and look for type match
- * Return: characters
+ * cleanup - Peforms cleanup operations for _printf.
+ * @args: A va_list of arguments provided to _printf.
+ * @output: A buffer_t struct.
+ */
+void cleanup(va_list args, buffer_t *output)
+{
+	va_end(args);
+	write(1, output->start, output->len);
+	free_buffer(output);
+}
+
+/**
+ * run_printf - Reads through the format string for _printf.
+ * @format: Character string to print - may contain directives.
+ * @output: A buffer_t struct containing a buffer.
+ * @args: A va_list of arguments.
+ *
+ * Return: The number of characters stored to output.
+ */
+int run_printf(const char *format, va_list args, buffer_t *output)
+{
+	int i, wid, prec, ret = 0;
+	char tmp;
+	unsigned char flags, len;
+	unsigned int (*f)(va_list, buffer_t *,
+			unsigned char, int, int, unsigned char);
+
+	for (i = 0; *(format + i); i++)
+	{
+		len = 0;
+		if (*(format + i) == '%')
+		{
+			tmp = 0;
+			flags = handle_flags(format + i + 1, &tmp);
+			wid = handle_width(args, format + i + tmp + 1, &tmp);
+			prec = handle_precision(args, format + i + tmp + 1,
+					&tmp);
+			len = handle_length(format + i + tmp + 1, &tmp);
+
+			f = handle_specifiers(format + i + tmp + 1);
+			if (f != NULL)
+			{
+				i += tmp + 1;
+				ret += f(args, output, flags, wid, prec, len);
+				continue;
+			}
+			else if (*(format + i + tmp + 1) == '\0')
+			{
+				ret = -1;
+				break;
+			}
+		}
+		ret += _memcpy(output, (format + i), 1);
+		i += (len != 0) ? 1 : 0;
+	}
+	cleanup(args, output);
+	return (ret);
+}
+
+/**
+ * _printf - Outputs a formatted string.
+ * @format: Character string to print - may contain directives.
+ *
+ * Return: The number of characters printed.
  */
 int _printf(const char *format, ...)
 {
-	operations_t ops[] = {
-	{'c', _putchar},
-	{'s', print_string},
-	{'d', print_integer},
-	{'i', print_integer},
-	{'r', reverse_string},
-	{'b', convert_binary},
-	{'\0', NULL}
-	};
+	buffer_t *output;
+	va_list args;
+	int ret;
 
-	va_list valist;
-	int i, j, x;
+	if (format == NULL)
+		return (-1);
+	output = init_buffer();
+	if (output == NULL)
+		return (-1);
 
-	j = 0;
-	va_start(valist, format);
+	va_start(args, format);
 
-	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-		return (0);
+	ret = run_printf(format, args, output);
 
-	for (i = 0; format[i]; i++)
-	{
-		for (x = 0; ops[x].type != '\0'; x++)
-		{
-			if (format[i] == '%')
-			{
-				if (format[i + 1] == ops[x].type)
-				{
-					i = i + 2;
-					j += ops[x].f(valist);
-				}
-				else if (format[i + 1] == '%')
-					i = i + 1;
-			}
-		}
-	j += print_char(format[i]);
-	}
-va_end(valist);
-return (j);
+	return (ret);
 }
